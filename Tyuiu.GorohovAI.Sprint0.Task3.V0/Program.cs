e55adce9c4a27ec9929b87b8af05b50d3c500567
Alexandr <39surara@gmail.com>
2024-09-22 17:05:23 +0500

    123123

---
+++ Tyuiu.GorohovAI.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.GorohovAI.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.GorohovAI.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнил: Горохов Александр Иванович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Оператор if - полная и короткая форма записи                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #0                                                            *");
+            Console.WriteLine("* Выполнил: Горохов Александр Иванович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу на, которая запрашивает целые значения с клавиатуры и *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*и.                                                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
