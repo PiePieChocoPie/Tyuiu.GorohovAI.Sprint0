78e6605dcae2d6f0890ca7a047599dc2adcffce3
Alexandr <39surara@gmail.com>
2024-09-22 22:45:50 +0500

    123123

--- Tyuiu.GorohovAI.Sprint0.Task7.V0.Lib/DataService.cs
+++ Tyuiu.GorohovAI.Sprint0.Task7.V0.Lib/DataService.cs
@@ -1,9 +1,16 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint0;
-
+﻿
 namespace Tyuiu.GorohovAI.Sprint0.Task7.V0.Lib
 {
-    public class DataService : ISprint0Task7V0
+    public class DataService
     {
-
+        public static int[] AdditionArray(int[] numOne, int[] numTwo)
+        {
+            int[] res = new int[5];
+            for (var i = 0; i < res.Length; i++)
+            {
+                res[i] = numOne[i] + numTwo[i];
+            }
+            return res;
+        }
     }
 }
